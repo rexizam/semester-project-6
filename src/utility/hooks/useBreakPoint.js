36217de95915a32725ebe7c479f663f91711aa11
rexizam <tomachan.psc@gmail.com>
2023-01-28 23:56:22 +0100

    fixed breakpoint for star rating component

--- src/utility/hooks/useBreakPoint.js
+++ src/utility/hooks/useBreakPoint.js
@@ -11,8 +11,8 @@ const breakpoints = {
 const useBreakpoint = () => {
   const [breakpoint, setBreakPoint] = useState('');
   const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
+    width: window.innerWidth,
+    height: window.innerHeight,
   });
 
   const handleResize = () => {
@@ -26,16 +26,16 @@ const useBreakpoint = () => {
     window.addEventListener('resize', handleResize);
     handleResize();
 
-    if (0 < windowSize.width && windowSize.width < 600) {
+    if (0 < windowSize.width && windowSize.width <= 600) {
       setBreakPoint(breakpoints[0]);
     }
-    if (600 < windowSize.width && windowSize.width < 960) {
+    if (600 < windowSize.width && windowSize.width <= 960) {
       setBreakPoint(breakpoints[600]);
     }
-    if (960 < windowSize.width && windowSize.width < 1280) {
+    if (960 < windowSize.width && windowSize.width <= 1280) {
       setBreakPoint(breakpoints[960]);
     }
-    if (1280 < windowSize.width && windowSize.width < 1920) {
+    if (1280 < windowSize.width && windowSize.width <= 1920) {
       setBreakPoint(breakpoints[1280]);
     }
     if (windowSize.width >= 1920) {
@@ -44,6 +44,7 @@ const useBreakpoint = () => {
 
     return () => window.removeEventListener('resize', handleResize);
   }, [windowSize.width]);
+
   return breakpoint;
 };
 
